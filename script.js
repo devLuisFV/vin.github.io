62784c51175603dc59b5d86f741734d773bfc3ca
devLuisFV <128266982+devLuisFV@users.noreply.github.com>
2023-06-01 00:22:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let menuVisible = false;
+//Funcion ocultar o mostrar menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = true;
+    }
+}
+function seleccionar(){
+    document.getElementById("nav").classList="";
+    menuVisible = false;
+}
+//Aplicar efecto a las animaciones
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills=window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+//Detectar el scrolling para aplicar la animacion
+window.onscroll = function(){
+    efectoHabilidades();
+}
